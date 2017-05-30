342e001a96dd77c4976ae1f1f33a2d0999775f54
Admin <admin@ElCapitan-iMac-Raum1.local>
2017-05-30 14:30:27 +0200

    finished logic

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,11 +1,10 @@
 "use strict;"
 // get location
-  let latitude = "";
-  let longitude = "";
-  let currentURL = "";
-  
 // ----- Google Maps Api Call --------
+
 function geoFindMe() {
+  let latitude;
+  let longitude;
   let output = document.getElementById("city");
 
   if (!navigator.geolocation){
@@ -16,65 +15,54 @@ function geoFindMe() {
   function success(position) {
     latitude  = position.coords.latitude;
     longitude = position.coords.longitude;
-
     //Location output
-    console.log(latitude)
-    output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+    //output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
     currentURL = "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&units=metric&APPID=93b0b9be965a11f0f099c8c7f74afa63";
 
-    console.log(city)
-    console.log(currentURL)
-
-
-    var img = new Image();
+   /* var img = new Image();
     img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
 
-    output.appendChild(img);
+    output.appendChild(img);*/
   
-        // vanilla js promises chain - openweather 
+    // ---------  vanilla js promise chain - openweather ----------
+  
     get(currentURL).then(function(response) {
       console.log('Vanilla Javascript success!', response.name);
       document.querySelector("#city").innerHTML = response.name + ", "  + response.sys.country + ":";
-      document.querySelector("#weather").innerHTML = response.main.temp
-      console.log(response.weather)
-
+      document.querySelector("#weather").innerHTML = response.main.temp;
+      console.log("Temp :", response.main.temp)
+      document.querySelector("#icon").innerHTML = '<img src="http://openweathermap.org/img/w/'+response.weather[0].icon+'.png">';
       
       // Event handlers
-
       let el = document.getElementById("unit");
       el.addEventListener("click", fireEvents);
 
       function fireEvents() {
         getUnit();
       }
-      // Temperature Unit change - promise call
+      // Temperature Unit change
         let clicked;
         let tempOutputCelsius = document.querySelector("#weather").innerHTML;
-        console.log("TempCelsius :",tempOutputCelsius)
         function getUnit() {
           let tempOutput = document.querySelector("#weather");
-          console.log("fired");
           clicked = !clicked;
-          console.log(clicked)
           if(clicked) {
-            console.log(tempOutput.innerHTML);
             tempOutput.innerHTML = Math.round(tempOutput.innerHTML * (9/5) + 32, -2);
-            
-            console.log(tempOutput.innerHTML);
+            document.getElementById("unit").innerHTML = ' <i class="wi wi-fahrenheit"></i>';
+            console.log(document.getElementById("unit").innerHTML);
           } else {
             tempOutput.innerHTML = tempOutputCelsius;
+            document.getElementById("unit").innerHTML = ' <i class="wi wi-celsius"></i>';
           }
           tempOutput.style.display = 'none';
-          tempOutput.style.display = 'block';
+          tempOutput.style.display = 'inline-block';
         } 
-
-
-
 
     }, function(error) {
       console.error('Vanilla Javascript failed!', error);
-    });
 
+    })
+    
 }
 
   function error() {
@@ -89,15 +77,11 @@ function geoFindMe() {
 // -------- End Google Maps Call 
 geoFindMe();
 
-console.log("lat :",latitude)
-    console.log("Current :",currentURL);
-
     // OPEN WEATHER API CALL -----------------  
    // Vanilla JavaScript OpenWeather query
 currentURL = "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&units=metric&APPID=93b0b9be965a11f0f099c8c7f74afa63";
 
-console.log(currentURL);
-// Vanilla Javascript Ajax call
+// Vanilla Javascript Ajax call (with promise)
 function get(url) {
   return new Promise(function(resolve, reject) {
     
