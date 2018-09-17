776e9f9edcb9bb6cb2b240f1795aa39c2631b817
Hartjenstein <hartjenstein@gmail.com>
2018-09-17 13:57:51 +0200

    *

--- src/js/weather.js
+++ src/js/weather.js
@@ -15,7 +15,7 @@ function geoFindMe() {
   function success(position) {
     latitude  = position.coords.latitude;
     longitude = position.coords.longitude;  
-    currentURL = "https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&units=metric&APPID=SECRET_OPENWEATHER_MAP&lang=de";
+    currentURL = "https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&units=metric&APPID=8cf55d0643deacaec6ffad3309ba7802&lang=de";
     let map = document.getElementById("google-map");
     var img = new Image();
     
@@ -98,7 +98,7 @@ geoFindMe();
 
     // OPEN WEATHER API CALL -----------------  
    // Vanilla JavaScript OpenWeather query
-currentURL = "https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&units=metric&APPID=SECRET_OPENWEATHER_MAP";
+currentURL = "https://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&units=metric&APPID=8cf55d0643deacaec6ffad3309ba7802";
 
 // Vanilla Javascript Ajax call (with promise)
 function get(url) {
